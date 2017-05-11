dd0e05a46ed1a6e78300f1fb049a6a4d2d74a932
lp <hlly_lp@163.com>
2017-05-11 21:50:02 +0800

    init

---
+++ maven02/src/main/java/com/shsxt/test/Test01.java
@@ -0,0 +1,14 @@
+package com.shsxt.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test01 {
+
+	@Test
+	public void test() {
+		System.out.println("git 测试。。。");
+	}
+
+}
